b517506e76467877c21f50471bebd5fc1e481b73
merto <mertenercan@gmail.com>
2023-10-14 13:39:03 +0300

    - project initial w/ additional configs

---
+++ types/TPokemon.ts
@@ -0,0 +1,36 @@
+// dispatch({
+//     type: "UPDATE",
+//     category:data4.genera[7].genus,
+//     growthRate: data4.growth_rate.name,
+//     catchRate:data4.capture_rate,
+//     icon: data3.sprites.other['official-artwork'].front_default,
+//     types: data3.types,
+//     eggGroups:data4.egg_groups,
+//     eggCycles:data4.hatch_counter,
+//     name: data3.name,
+//     moves: data3.moves,
+//     abilities:data3.abilities,
+//     mainType: data3.types[0].type.name,
+//     baseExp: data3.base_experience,
+//     stats: data3.stats,
+//     heldItems:data3.held_items,
+//     physicalStats: {weight:data3.weight, height:data3.height},
+//     baseFormName:data5.chain.species.name,
+//     firstEvolutionName: data5.chain.evolves_to[0]?.species.name,
+//     secondEvolutionName: data5.chain.evolves_to[0]?.evolves_to[0]?.species.name,
+//     firstEvolutionLevel:data5.chain.evolves_to[0]?.evolution_details[0].min_level,
+//     firstEvolutionItem: data5.chain.evolves_to[0]?.evolution_details[0].item,
+//     firstEvolutionIcon:data6.data?.sprites.other['official-artwork'].front_default,
+//     secondEvolutionIcon: data7.data?.sprites.other['official-artwork'].front_default,
+//     secondEvolutionItem: data5.chain.evolves_to[0]?.evolves_to[0]?.evolution_details[0].item,
+//     firstEvolutionTrigger:data5.chain.evolves_to[0]?.evolution_details[0].trigger,
+//     secondEvolutionTrigger:data5.chain.evolves_to[0]?.evolves_to[0]?.evolution_details[0].trigger,
+//     secondEvolutionHapiness:data5.chain.evolves_to[0]?.evolves_to[0]?.evolution_details[0].min_happiness,
+//     firstEvolutionHapiness:data5.chain.evolves_to[0]?.evolution_details[0].min_happiness,
+//     secondEvolutionLevel : data5.chain.evolves_to[0]?.evolves_to[0]?.evolution_details[0].min_level,
+//     baseFormIcon:data8.data.sprites.other['official-artwork'].front_default
+    
+// })
+
+
+// }
